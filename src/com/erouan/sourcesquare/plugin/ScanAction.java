89b2fc02670b6ef8af1d3c103ce6cd9b43d5222f
Antoine ROUAZE <antoine.rouaze@gmail.com>
2012-06-12 18:02:00 +0200

    Update ScanAction: This is the project that is scanned, not libraries.

--- src/com/erouan/sourcesquare/plugin/ScanAction.java
+++ src/com/erouan/sourcesquare/plugin/ScanAction.java
@@ -1,5 +1,22 @@
 package com.erouan.sourcesquare.plugin;
 
+import com.antelink.sourcesquare.client.scan.ScanStatus;
+import com.antelink.sourcesquare.client.scan.ScanStatusManager;
+import com.antelink.sourcesquare.client.scan.SourceSquareEngine;
+import com.antelink.sourcesquare.client.scan.SourceSquareFSWalker;
+import com.antelink.sourcesquare.event.base.ClientEventHandler;
+import com.antelink.sourcesquare.event.base.ClientEventType;
+import com.antelink.sourcesquare.event.base.EventBus;
+import com.antelink.sourcesquare.event.events.FilesScannedEvent;
+import com.antelink.sourcesquare.event.events.OSFilesFoundEvent;
+import com.antelink.sourcesquare.event.events.ScanCompleteEvent;
+import com.antelink.sourcesquare.event.events.StartScanEvent;
+import com.antelink.sourcesquare.event.handlers.FilesScannedEventHandler;
+import com.antelink.sourcesquare.event.handlers.OSFilesFoundEventHandler;
+import com.antelink.sourcesquare.event.handlers.ScanCompleteEventHandler;
+import com.antelink.sourcesquare.query.AntepediaQuery;
+import com.antelink.sourcesquare.results.ResultBuilder;
+import com.antelink.sourcesquare.results.TreeMapBuilder;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.module.Module;
@@ -8,8 +25,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.LibraryOrderEntry;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.OrderEntry;
+import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.VirtualFile;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,21 +40,46 @@ import java.util.Set;
  */
 public class ScanAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        Set<LibraryOrderEntry> libraries = new HashSet<LibraryOrderEntry>();
-        Module[] modules = ModuleManager.getInstance(e.getProject()).getModules();
-        for (Module module : modules) {
-            OrderEntry[] orderEntries = ModuleRootManager.getInstance(module).getOrderEntries();
-            for (OrderEntry orderEntry : orderEntries) {
-                if (orderEntry instanceof LibraryOrderEntry) {
-                    LibraryOrderEntry libraryOrderEntry = (LibraryOrderEntry) orderEntry;
-                    libraries.add(libraryOrderEntry);
-                }
-            }
-        }
-        System.out.println(libraries.size());
+        Project project = e.getProject();
+        String projectPath = project.getBasePath();
+        startScan(projectPath);
     }
 
-    private void startScan(Set<LibraryOrderEntry> libraries) {
+    private void startScan(String projectPath) {
+        final EventBus eventBus = new EventBus();
+        AntepediaQuery query = new AntepediaQuery();
+        SourceSquareEngine engine = new SourceSquareEngine(eventBus, query);
+        ScanStatusManager manager = new ScanStatusManager(eventBus);
+        manager.bind();
+        TreeMapBuilder treemap = new TreeMapBuilder(eventBus);
+        treemap.bind();
+        ResultBuilder builder = new ResultBuilder(eventBus, treemap);
+        builder.bind();
+        final SourceSquareFSWalker walker = new SourceSquareFSWalker(engine, eventBus, treemap);
+        walker.bind();
+        File file = new File(projectPath);
+        eventBus.fireEvent(new StartScanEvent(file));
+        eventBus.addHandler(ScanCompleteEvent.TYPE, new ScanCompleteEventHandler() {
+            @Override
+            public void handle(ArrayList<Integer> levels) {
+                System.out.println("Files scanned at the end: " + ScanStatus.INSTANCE.getNbFilesToScan());
+            }
 
+            @Override
+            public String getId() {
+                return getClass().getCanonicalName() + ": " + ScanCompleteEventHandler.class.getName();
+            }
+        });
+        eventBus.addHandler(FilesScannedEvent.TYPE, new FilesScannedEventHandler() {
+            @Override
+            public void handle(int count) {
+                System.out.println("Files scanned: " + ScanStatus.INSTANCE.getNbFilesScanned());
+            }
+
+            @Override
+            public String getId() {
+                return getClass().getCanonicalName() + ": " + FilesScannedEventHandler.class.getName();
+            }
+        });
     }
 }
